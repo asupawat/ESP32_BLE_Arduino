a2bbc52bd5f120b8d71691ca8fc29d6c4ef91b28
Nishanth Samala <nishanth.samala@gmail.com>
2018-07-30 14:53:18 -0500

    remove BLENotifier header file

--- src/BLERemoteCharacteristic.h
+++ src/BLERemoteCharacteristic.h
@@ -19,6 +19,11 @@
 #include "BLEUUID.h"
 #include "FreeRTOS.h"
 
+class BLENotifier {
+	public:
+		virtual void onNotify(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify)=0;
+};
+
 class BLERemoteService;
 class BLERemoteDescriptor;
 
